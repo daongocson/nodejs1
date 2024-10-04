9fdcf4e0f9a0fe128bf6d91b672a6066040c3be8
SON DAO <ngocsonit@gmai.com>
2024-10-04 09:32:01 +0700

    fist

---
+++ src/middleware/delay.js
@@ -0,0 +1,14 @@
+
+const delay = (req, res, next) => {
+    setTimeout(() => {
+        if (req.headers.authorization) {
+            const token = req.headers.authorization.split(' ')[1];
+            console.log(">>> check token: ", token)
+        }
+
+
+        next()
+    }, 30)
+}
+
+module.exports = delay;
