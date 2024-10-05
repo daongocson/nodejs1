153155efd3d9eb5731061e6a0a9f2f91d867838f
SON DAO <ngocsonit@gmai.com>
2024-10-05 09:26:08 +0700

    ab

--- src/config/datasql.js
+++ src/config/datasql.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const sqlConfig = {
     user: 'sa',
     password: '123@lrco',
-    server: '192.168.8.21', // You can use 'localhost\\instance' to connect to named instance
+    server: '113.160.170.53', // You can use 'localhost\\instance' to connect to named instance
     database: 'His_xml',
     options: {
         encrypt: false, // Disable SSL/TLS
